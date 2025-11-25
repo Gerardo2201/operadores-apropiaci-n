e1b01a359609cd2783798c593990e6bad369289d
Jhon Gelvez <jhonjairogelvezgomez@gmail.com>
2025-11-25 18:01:56 -0500

    Se inicializan los ejercicios y se avanza hasta el 2do

---
+++ app.js
@@ -0,0 +1,24 @@
+// Ejercicio 1 El terreno rectangular
+//Un agricultor tiene un terreno rectangular y conoce su base y su altura en metros. Quiere calcular tanto el área como el perímetro del terreno para cercarlo y sembrar. Diseña un programa en el que él pueda obtener ambos valores.
+
+let base = 150;
+let altura = 100;
+
+let area = base * altura;
+let perimetro = area * 2;
+
+console.log("El area del terreno es: " + area + "\nEl perimetro del terreno es: " + perimetro);
+
+// Ejercicio 2 Las tres notas del estudiante
+
+// Un aprendiz obtuvo tres notas en sus evaluaciones. Necesita calcular el promedio para saber cómo le fue en la asignatura. Crea un programa que, a partir de tres valores numéricos, muestre el promedio en pantalla.
+
+let nota1 = 4;
+let nota2 = 5;
+let nota3 = 3.5;
+
+let notas = nota1 + nota2 + nota3;
+
+let promedio = notas / 3;
+
+console.log("El promedio es:" + promedio);
